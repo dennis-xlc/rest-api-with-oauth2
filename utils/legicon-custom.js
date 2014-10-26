3efc0f56cbdbcdfa40e768d4154a70ac6236a6b6
Xu Lvcai <xulvcai2008@gmail.com>
2014-10-27 00:48:46 +0800

    change the bg color of legicon

--- utils/legicon-custom.js
+++ utils/legicon-custom.js
@@ -134,7 +134,7 @@ module.exports = function Legicon (key, options) {
                 defaults.size = defaults.square * defaults.grid + defaults.padding * 2;
 
                 // Background color
-                defaults.bgColor = "rgb(254,254,254)";//"#F5F5F5";
+                defaults.bgColor = "rgb(245,245,245)";//"#F5F5F5";
 
                 // Fill color override
                 defaults.fillColor = void 0;
